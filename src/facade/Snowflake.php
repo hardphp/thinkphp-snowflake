90e145d696cdc72425c73f5ae8a0fb3d70ac0c45
hardphp <hardphp@163.com>
2020-01-03 17:14:44 +0800

    Add files via upload

--- src/facade/Snowflake.php
+++ src/facade/Snowflake.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace think\facade;
+
+use think\Facade;
+
+/**
+ * Class Snowflake
+ * @package think\facade
+ */
+class Snowflake extends Facade
+{
+    protected static function getFacadeClass()
+    {
+        return 'snowflake';
+    }
+}
